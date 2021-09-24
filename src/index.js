5c890c9d34159b325a03a2dcca55960cf61c6528
PuneetGopinath <baalkrshna@gmail.com>
2021-09-24 11:46:12 +0530

    fix(db): connect to mongo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 require("dotenv").config();
 const DisList = require("./DisList");
 const client = new DisList();
+const mongoose = require("mongoose");
+// eslint-disable-next-line no-undef
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log("Connected to MongoDB!");
+    })
+    .catch((err) => {
+        console.error("Error connecting to MongoDB: ", err);
+    });
 process.on("unhandledRejection", (error) => {
     client.logger.log("Unhandled promise rejection", "error");
     console.error(error);

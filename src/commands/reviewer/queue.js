74b6ec8aeec0acf50bdc3a4f6efac57aee3403ed
PuneetGopinath <baalkrshna@gmail.com>
2021-09-25 18:34:19 +0530

    refactor(queue): show all bots

--- src/commands/reviewer/queue.js
+++ src/commands/reviewer/queue.js
@@ -1,5 +1,6 @@
 //eslint-disable-next-line no-unused-vars
 const { Command } = require("../../structures");
+const { Pagination } = require("djs-pagination-buttons");
 const { MessageEmbed } = require("discord.js");
 module.exports = class CMD extends Command {
     constructor(client) {
@@ -17,11 +18,26 @@ module.exports = class CMD extends Command {
 
     async execute({ message }) {
         const queue = await this.client.models.Bot.find({ approved: false });
+        const pages = [new MessageEmbed()];
 
-        const embed = new MessageEmbed()
+        pages[0]
+            .setColor(`BLURPLE`)
             .setAuthor(`DisList Queue`)
-            .setDescription(`There are currently **${queue.length}** bot(s) awaiting approval.`)
-            .setColor(`BLURPLE`);
-        message.channel.send({ embeds: [embed] });
+            .setDescription(
+                `There are currently **${queue.length}** bot(s) awaiting approval.\nGo to the next page to start the list of bots`
+            );
+        queue.forEach(async (botDB) => {
+            const bot = await this.client.users.fetch(botDB.botId);
+            const p = pages.length;
+            pages[p] = new MessageEmbed()
+                .setColor(`BLURPLE`)
+                .setTitle(`${bot.username}`)
+                .setDescription(botDB.descriptions.short)
+                .setURL(`${this.client.config.site.url}/bot/${bot.id}`);
+        });
+        const pagination = new Pagination(this.client);
+        pagination.setPages(pages);
+        pagination.setAuthorizedUsers([message.author.id]);
+        pagination.send(message.channel);
     }
 };

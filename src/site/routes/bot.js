d1c1d749c6e54b88fe1f19d7c1927d17441d3d4c
PuneetGopinath <baalkrshna@gmail.com>
2021-09-26 16:42:07 +0530

    fix(bot router)

--- src/site/routes/bot.js
+++ src/site/routes/bot.js
@@ -3,6 +3,7 @@
  * Copyright (c) 2021 The DisList Team and Contributors
  * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
  */
+const axios = require("axios");
 const { CheckAuth } = global;
 const express = require("express");
 const router = express.Router();

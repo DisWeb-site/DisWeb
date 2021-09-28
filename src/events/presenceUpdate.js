d89b63d5b55883a657563e2a4cd5ec175d9925a2
PuneetGopinath <baalkrshna@gmail.com>
2021-09-28 22:54:23 +0530

    fix(presenceUpdate)

--- src/events/presenceUpdate.js
+++ src/events/presenceUpdate.js
@@ -15,25 +15,26 @@ const { MessageEmbed } = require("discord.js");
         let rate = botDB.uptime?.rate ?? 99;
         const uptimeLogs = await client.channels.fetch(client.config.channels.uptimeLogs);
         const makeEmbed = () => {
-            const embed = new MessageEmbed(botStatus)
+            const em = new MessageEmbed(botStatus)
                 .setAuthor(user.tag, user.displayAvatarURL())
                 .setTitle(`${client.config.emojis?.[botStatus]} Your bot ${user.tag} ${botStatus === "offline" ? "went offline" : "came back online"}!`)
                 .setDescription(`It is **${user}** this bot!`)
                 .addField("**Uptime Rate**", rate);
-            return embed;
+            return em;
         }
+        let embed, msg;
         switch(newPresence?.status?.toLowerCase?.()) {
             case "offline":
-                const embed = makeEmbed("offline");
-                const msg = await uptimeLogs.send({ embeds: [embed] });
+                embed = makeEmbed("offline");
+                msg = await uptimeLogs.send({ embeds: [embed] });
                 botDB.uptime.log = msg.id;
                 botDB.uptime.lastOfflineAt = Date.now();
                 break;
             case "online":
             case "dnd":
             case "idle":
-                const embed = makeEmbed("online");
-                const msg = await uptimeLogs.messages.fetch(`${botDB.uptime.log}`);
+                embed = makeEmbed("online");
+                msg = await uptimeLogs.messages.fetch(`${botDB.uptime.log}`);
                 msg.edit({ embeds: [embed] }).catch(e => msg.channel.send({ embeds: [embed] }));
                 botDB.uptime.log = null;
                 botDB.uptime.lastOfflineAt = null;

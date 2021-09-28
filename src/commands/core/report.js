98361ce1ed302a60af9989e05b74deed015dd4cc
PuneetGopinath <baalkrshna@gmail.com>
2021-09-28 17:23:17 +0530

    refactor(report): fix report

--- src/commands/core/report.js
+++ src/commands/core/report.js
@@ -1,39 +1,54 @@
+/**
+ * UpList
+ * Copyright (c) 2021 The UpList Team and Contributors
+ * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
+ */
 const { Command } = require("../../structures");
-const { MessageEmbed } = require('discord.js');
-
+const { MessageEmbed } = require("discord.js");
 module.exports = class CMD extends Command {
-  constructor(client) {
-    super(
-      {
-        name: "report",
-        description: "Report issues.",
-        disabled: false,
-        category: "Core"
-      },
-      client
-    );
-  }
-
-  async execute({ message }) {
-      const prefix = "=";
-      const args = message.content.slice(prefix.length).trim().split(/ +/g);
-      const report = args.join(" ");
-      if (!report) return message.channel.send(`Please give some feedback to send the report.`)
-      if (report < 10) 
-        message.channel.send(`Your report must have at least **10** characters.`);
-      const UpList = client.guilds.cache.get('PUT GUILD ID HERE');
-      const UpListReportChannel = UpList.channels.cache.get('PUT REPORT CHANNEL ID HERE');
-      
-      if (UpListReportChannel) {
-        const report = new MessageEmbed();
-        report.setAuthor(`Report`);
-        report.setDescription(`**From**: ${message.author.tag}
-**Report**: \`${report}\``);
-        report.setFooter(`Reported by ${message.author.tag}`);
-        UpListReportChannel.send(report);
-        if (reason) {
-          message.channel.send(`:white_check_mark: Your report has been submitted.`)
-        }
-      }
+    constructor(client) {
+        super(
+            {
+                name: "report",
+                description: "Report issues.",
+                disabled: false,
+                category: "Core",
+            },
+            client
+        );
     }
-}
+
+    async execute({ message, args }) {
+        const { config } = this.client;
+        const report = args.join(" ");
+        if (!report)
+            return message.channel.send(
+                "Please give some feedback to send the report."
+            );
+        if (report.length < 5)
+            message.channel.send(
+                "Your report must have at least **5** characters."
+            );
+        const reportLogs = await this.client.channels.fetch(
+            config.channels.reportLogs
+        );
+
+        const embed = new MessageEmbed()
+            .setAuthor(message.author.tag, message.author.displayAvatarURL())
+            .setTitle("New report 🐛")
+            .setDescription(report)
+            .addField(
+                "**From guild:**",
+                `${message.guild.name} (${message.guild.id})`
+            )
+            .addField(
+                "**Reporter:**",
+                `<@${message.author.id}> (${message.author.id})`
+            )
+            .setFooter(`${message.author.tag} made a new report!`);
+        reportLogs.send({ embeds: [embed] });
+        message.channel.send(
+            ":white_check_mark: Your report has been submitted."
+        );
+    }
+};

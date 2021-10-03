2d8d4ed095c16c6a43f91db88f597025b05e5df3
PuneetGopinath <baalkrshna@gmail.com>
2021-10-03 12:47:33 +0530

    fix(apiLimiter): add comment for easy understanding

--- src/site/controllers/apiLimiter.js
+++ src/site/controllers/apiLimiter.js
@@ -2,5 +2,5 @@ const rateLimit = require("express-rate-limit");
 
 module.exports = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 60,
+    max: 60, // limit each IP to 60 requests per windowMs
 });

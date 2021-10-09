cdd6ca6fad3bb43f3b054a312c6cf14c2f278207
PuneetGopinath <baalkrshna@gmail.com>
2021-10-09 15:27:13 +0530

    fix(disweb): add dm intent

--- src/DisWeb.js
+++ src/DisWeb.js
@@ -19,6 +19,7 @@ class DisWeb extends Client {
                 Intents.FLAGS.GUILDS,
                 Intents.FLAGS.GUILD_MESSAGES,
                 Intents.FLAGS.GUILD_PRESENCES,
+                Intents.FLAGS.DIRECT_MESSAGES,
             ],
             partials: ["CHANNEL"],
         });
